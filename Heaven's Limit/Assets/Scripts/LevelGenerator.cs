9a187fe75c12741c89d1085518e9ff894f1eb079
sooyaaa0801 <72476689+sooyaaa0801@users.noreply.github.com>
2021-01-28 17:46:35 +0800

    added heaven's limit

---
+++ Heaven's Limit/Assets/Scripts/LevelGenerator.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelGenerator : MonoBehaviour
+{
+
+    public GameObject platformPrefab;
+    public GameObject platformPrefab2;
+
+    public int numberOfPlatforms;
+    public float levelWidth = 3f;
+    public float color;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Vector3 spawnPosition = new Vector3();
+
+        for (int i = 0; i < numberOfPlatforms; i++)
+        {
+            spawnPosition.y += 0.57f;
+            color = Random.Range(1, 3);
+            if (color <= 1)
+            {
+                Instantiate(platformPrefab, spawnPosition, Quaternion.identity);
+            }
+            else if (color <= 2)
+            {
+                Instantiate(platformPrefab2, spawnPosition, Quaternion.identity);
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Heaven's Limit/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public float jumpSpeed = 3.5f;
+    private Rigidbody2D rigidBody;
+    public Transform groundCheckPoint;
+    public float groundCheckRadius;
+    public LayerMask groundLayer;
+    private bool isTouchingGround;
+
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        isTouchingGround = Physics2D.OverlapCircle(groundCheckPoint.position, groundCheckRadius, groundLayer);
+        
+        if(Input.GetButtonDown("Jump") && isTouchingGround)
+        {
+            rigidBody.velocity = new Vector2(0, jumpSpeed);
+        }
+    }
+}
